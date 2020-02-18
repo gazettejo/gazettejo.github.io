d547f4a8ccfb011df76f452e520d03cf958ae032
jz360 <jacqueshzhang97@gmail.com>
2020-02-17 23:44:59 -0500

    changed images + added click me on jo img feature

---
+++ assets/js/myscript.js
@@ -0,0 +1,33 @@
+function changeImage(clicked_id) {
+	var MAX_NUM_IMG_PER_PORTION = 3;
+	
+	var portion_index = 3;
+	if(clicked_id == "top_img"){
+		portion_index = 1;
+	} else if (clicked_id == "mid_img"){
+		portion_index = 2;
+	} 
+	
+	var arr = document.getElementById(clicked_id).src.split("/");
+	var img_file_name = arr[arr.length - 1];
+	var img_index = parseInt(img_file_name.charAt(img_file_name.length - 5));
+	
+	document.getElementById(clicked_id).src = ("images/switch/").concat(portion_index, ".", img_index == MAX_NUM_IMG_PER_PORTION ? 1 : img_index + 1, ".jpg");
+}
+
+function changeImageEN(clicked_id) {
+	var MAX_NUM_IMG_PER_PORTION = 3;
+	
+	var portion_index = 3;
+	if(clicked_id == "top_img_en"){
+		portion_index = 1;
+	} else if (clicked_id == "mid_img_en"){
+		portion_index = 2;
+	} 
+	
+	var arr = document.getElementById(clicked_id).src.split("/");
+	var img_file_name = arr[arr.length - 1];
+	var img_index = parseInt(img_file_name.charAt(img_file_name.length - 5));
+	
+	document.getElementById(clicked_id).src = ("images/switch/").concat(portion_index == 3 ? "en" : "", portion_index, ".", img_index == MAX_NUM_IMG_PER_PORTION ? 1 : img_index + 1, ".jpg");
+}
